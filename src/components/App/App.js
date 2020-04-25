babb02a96600eeb92ba8dbfa55f798e1c051a576
Courtney Overby <brioverby@gmail.com>
2020-04-25 15:29:39 -0500

    error in index.js, components set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { HashRouter as Router, Route } from "react-router-dom";
+
+import PageHome from "../PageHome/PageHome";
+import PageDetails from "../PageDetails/PageDetails";
+import PageEdit from "../PageEdit/PageEdit";
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Router>
+          <Route exact path="/" component={PageHome} />
+          <Route exact path="/details/:id" component={PageDetails} />
+          <Route exact path="/details/:id/edit" component={PageEdit} />
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
