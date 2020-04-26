56783871103ffbd27d0d4e22c13e634caa3e178b
Courtney Overby <brioverby@gmail.com>
2020-04-26 11:43:52 -0500

    GET route to retrieve data from movie table created in movie.router

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const pool = require("../modules/pool");
 
+// Create route to get data from table.
+// Create route to get genre from table.
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const queryText = `SELECT * FROM movies ORDER BY id`;
+
+  pool
+    .query(queryText)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((error) => {
+      console.warn(error);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
