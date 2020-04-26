9bc00f5e78acfb05fc02129b761dfc2fc5341b69
Courtney Overby <brioverby@gmail.com>
2020-04-26 11:33:39 -0500

    added server routes before adding a router

--- src/components/Pages/PageMovies/MovieItem/MovieItem.js
+++ src/components/Pages/PageMovies/MovieItem/MovieItem.js
@@ -3,7 +3,13 @@ import { withRouter } from "react-router-dom";
 
 class MovieItem extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div>image={this.props.movie.poster}</div>
+        <h1>{this.props.movie.title}</h1>
+        <div>{this.props.movie.description}</div>
+      </div>
+    );
   }
 }
 
